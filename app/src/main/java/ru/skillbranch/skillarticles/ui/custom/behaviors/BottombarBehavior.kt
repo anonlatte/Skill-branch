0652d09ff46f280df33567035f5d4595d74f9135
German Proshunin <proshunin.german@yandex.ru>
2021-04-25 16:04:34 +0500

    fix animation calculations

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
@@ -25,7 +25,8 @@ class BottombarBehavior : CoordinatorLayout.Behavior<Bottombar>() {
         consumed: IntArray,
         type: Int
     ) {
-        child.translationY = MathUtils.clamp(child.translationY + dy, 0f, child.minHeight.toFloat())
+        val offset = MathUtils.clamp(child.translationY + dy, 0f, child.minHeight.toFloat())
+        if (offset != child.translationY) child.translationY = offset
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
     }
 }
