1398344e790cdd60c398bc97fc9a85fe8cadea42
German Proshunin <proshunin.german@yandex.ru>
2021-04-25 13:37:51 +0500

    fix for server compiler

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/BaseViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/BaseViewModel.kt
@@ -50,7 +50,7 @@ abstract class BaseViewModel<T>(initState: T) : ViewModel() {
      * выражение обрабатывающее изменение текущего стостояния
      */
     fun observeState(owner: LifecycleOwner, onChanged: (newState: T) -> Unit) {
-        state.observe(owner, { onChanged(it!!) })
+        state.observe(owner, Observer { onChanged(it!!) })
     }
 
     /***
